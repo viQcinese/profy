8e9c0138914f670de4a41086fd782b6d23e49629
Vitor Senise <vitor.senise@gmail.com>
2020-08-06 03:18:50 -0300

    End you day 3

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -2,35 +2,61 @@ import React from 'react'
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
 
+import api from '../../services/api'
+
 import './styles.css'
 
-function TeacherItem() {
+export interface Teacher {
+    id: number,
+    avatar: string,
+    bio: string,
+    cost: number,
+    name: string,
+    subject: string,
+    whatsapp: string
+}
+
+interface TeacherItemProps {
+  teacher: Teacher
+}
+
+const TeacherItem: React.FunctionComponent<TeacherItemProps> = ({ teacher }) => {
+
+  function createNewConnection() {
+    api.post('/connections', {
+      user_id: teacher.id
+    })
+  }
+
   return (
     <article className="teacher-item">
 
       <header>
-        <img src="https://avatars2.githubusercontent.com/u/65465930?s=460&u=f62ac43766224a5e661d78b2d39ba469ac3de51c&v=4" alt="Vitor Senise"/>
+        <img src={teacher.avatar} alt={teacher.name}/>
 
         <div>
-          <strong>Vitor Senise</strong>
-          <span>Filosofia</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
 
       <p>
-        Um textão muito louco com muitas coisas loucas <br/>
-        Mais texto. Olha só quanto texto. Me texto, é texto demais, Brasil...
+        {teacher.bio}
       </p>
 
       <footer>
         <p>
           Preço/hora 
-          <strong>R$50,00</strong>
+          <strong>R$ {teacher.cost}</strong>
         </p>
-        <button type="button">
+        <a 
+          onClick={createNewConnection} 
+          href={`https://wa.me/${teacher.whatsapp}`}
+          target="_blank"
+        >
           <img src={whatsappIcon} alt="Whatsapp" />
           Entrar em contato
-        </button>
+        </a>
       </footer>
     </article>
   )
